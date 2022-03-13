51ef70a8fe55bfbced728d68290567eca428e7b5
Beyza Aydın <52197142+beyzaydnn@users.noreply.github.com>
2022-03-14 02:10:51 +0300

    Add files via upload

---
+++ Day2/Task1/src/Instructer.java
@@ -0,0 +1,20 @@
+
+public class Instructer {
+	int id;
+	String firstName;
+	String lastName;
+	
+	
+	public Instructer() {
+		
+	}
+
+
+	public Instructer(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+}
---
+++ Day2/Task1/src/Lecture.java
@@ -0,0 +1,19 @@
+
+public class Lecture {
+	int id;
+	String name;
+	String title;
+	String price;
+	
+	public Lecture() {
+		
+	}
+	public Lecture(int id, String name, String title, String price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.title = title;
+		this.price = price;
+	}
+	
+}
